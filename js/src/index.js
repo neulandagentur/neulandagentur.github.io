971a969afa57422c263efefadbe1d097eb90249e
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-03-27 07:39:06 +0100

    Remove test js

--- js/src/index.js
+++ js/src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+
+    const request =  new XMLHttpRequest();
+    request.open('GET', `https://api.github.com/users/neulandagentur/repos`, true);
+    request.addEventListener('load', () => {
+        let data = null;
+        if (request.status > 200 || request.status <= 300) {
+            data = JSON.parse(request.responseText);
+            data.forEach((repo) => {
+                console.log(repo.name);
+            })
+        } else console.log('error');
+    });
+    request.send();
