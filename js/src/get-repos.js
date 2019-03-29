436fb2faa3ab4c635babcca9e72c4de2fdb1a03b
AlexPrediger <39220443+AlexPrediger@users.noreply.github.com>
2019-03-29 13:36:56 +0100

    filter complete and code revise

--- js/src/get-repos.js
+++ js/src/get-repos.js
@@ -1,112 +1,122 @@
 import h from './h';
+import Repository from './Repository';
+
+/**
+ * current state
+ * @type {Object}
+ */
+const state = {
+  activeItem: null,
+  activeLanguage: null,
+  items: []
+};
+
+
 
 export class Repos {
   constructor() {
     this.template = null;
     this.languages = [];
-    this.getRepos(this.createFilter());
+    this.request = null;
+    this.data = null;
+    this.fetchData(() => {
+      this.renderListItems();
+      this.renderFilter();
+    });
   }
 
-  getRepos(callback) {
-    const request = new XMLHttpRequest();
-    this.request = request;
+  fetchData(callback) {
+    this.request = new XMLHttpRequest();
     this.request.open('GET', 'https://api.github.com/users/neulandagentur/repos');
     this.request.addEventListener('load', () => {
-      let data;
       if (this.request.status > 200 || this.request.status <= 300) {
-        data = JSON.parse(this.request.responseText);
-        data.forEach((repo) => {
-          if (repo.private === 'false') {
-            return;
-          }
-          let license = null;
-          if (repo.license) {
-            license = repo.license.spdx_id;
-          }
-          this.render({
-            title: repo.name,
-            description: repo.description,
-            forks: repo.forks_count,
-            stars: repo.stargazers_count,
-            license,
-            language: repo.language,
-            url: repo.html_url,
-          });
-        });
-      } else console.log('error');
+        this.data = JSON.parse(this.request.responseText);
+        if (callback) {
+          callback();
+        }
+      } else {
+        console.error(this.request.responseText);
+      }
     });
     this.request.send();
-    if(callback) callback();
   }
 
-  render(repository = {}) {
-    const template = this.createTemplate(repository);
-    document.querySelector('.projects').appendChild(template);
-  }
+  renderListItems() {
+    this.data.forEach((repo) => {
 
-  createFilter() {
-    const template = h('ul', { class: 'filter-list' });
-    document.querySelector('.language-filter').appendChild(template);
-    this.createListItems();
-  }
+      const license = repo.license
+        ? repo.license.spdx_id
+        : null;
 
-  addListener() {
-    const items = document.querySelectorAll('.filter-item');
-    items.forEach((listLanguage) => {
-      listLanguage.addEventListener('click', () => {
-        if (listLanguage.classList.contains('is--active')) {
-          const projects = document.querySelectorAll('.project')
-          projects.forEach((project) => {
-            project.style.display = 'block';
-            listLanguage.classList.toggle('is--active');
-          })
-        } else {
-          const projectLanguages = document.querySelectorAll('[data-language]');
-          projectLanguages.forEach((projectLanguage) => {
-            if (projectLanguage.innerHTML !== listLanguage.innerHTML) {
-              projectLanguage.parentNode.parentNode.style.display = 'none';
-            } else {
-              projectLanguage.parentNode.parentNode.style.display = 'block';
-            }
-          })
-          listLanguage.classList.toggle('is--active');
-        }
-      })
-    })
-  }
+      const repoInstance = new Repository(
+        repo.name,
+        repo.description,
+        repo.forks_count,
+        repo.stargazers_count,
+        license,
+        repo.language,
+        repo.html_url
+      );
 
-  createListItems() {
-    console.log(this.languages);
-    this.languages.forEach((language) => {
-      const listItem = h('li', { class: 'filter-item' }, [language]);
-      document.querySelector('.filter-list').appendChild(listItem);
+      const template = repoInstance.createTemplate();
+      state.items.push({language: repo.language, item: template});
+      document.querySelector('.projects').appendChild(template);
     });
-    this.addListener();
   }
 
-  createTemplate(repository = {}) {
-    const language = repository.language || 'no language';
+  filterUniqueLanguage() {
+    const uniqueLanguage = [];
 
-    if (this.languages.indexOf(language) < 0) {
-      this.languages.push(language);
+    this.data.forEach((repo) => {
+      const language = repo.language;
+
+      if (uniqueLanguage.indexOf(language) < 0){
+        uniqueLanguage.push(language);
+      }
+    });
+
+    return uniqueLanguage;
+  }
+
+  renderFilter() {
+    const filterItems = this
+      .filterUniqueLanguage()
+      .map(language => h('li', { class: 'filter-item', click: (e, el) => { this.filterAction(language, el) }}, [language]));
+
+    const filter = h('ul', {class: 'filter-list'}, filterItems);
+
+    document.querySelector('.language-filter').appendChild(filter);
+  }
+
+  filterAction(language, filterItem) {
+    if (state.activeLanguage === null) {
+      state.items.forEach((item) => {
+        if (item.language !== language) {
+          item.item.style.display = 'none';
+        }
+      });
+      state.activeLanguage = language;
+      filterItem.classList.add('is--active');
+      state.activeItem = filterItem;
+
+    } else if (state.activeLanguage === language) {
+      state.items.forEach((item) => { item.item.style.display = 'block' });
+      state.activeLanguage = null;
+      state.activeItem.classList.remove('is--active');
+      state.activeItem = null;
+    } else {
+      state.items.forEach((item) => {
+        if (item.language === language) {
+          item.item.style.display = 'block';
+        } else {
+          item.item.style.display = 'none';
+        }
+      });
+
+      filterItem.classList.add('is--active');
+      state.activeItem.classList.remove('is--active');
+      state.activeItem = filterItem;
+      state.activeLanguage = language;
     }
-
-    const license = repository.license
-      ? h('div', { class: 'license'}, [repository.license.toString()])
-      : null;
-    this.template = h('div', { class: 'project' }, [
-      h('div', { class: 'inner' }, [
-        h('div', { class: 'head' }, [
-          h('div', { class: 'stars' }, [repository.stars.toString()]),
-          h('div', { class: 'forks' }, [repository.forks.toString()]),
-          license,
-        ]),
-        h('div', { class: 'language', 'data-language': language.toLowerCase() }, [language]),
-        h('h3', { class: 'title' }, [repository.title]),
-        h('p', { class: 'description' }, [repository.description]),
-        h('a', { class: 'link', href: repository.url }, [repository.url.toString()]),
-      ]),
-    ]);
-    return this.template;
   }
 }
---
+++ js/src/repository.js
@@ -0,0 +1,37 @@
+import h from './h';
+
+export default class Repository {
+
+  constructor(title, description, forks, stars, license, language, url) {
+    this.title = title;
+    this.description = description;
+    this.forks = forks;
+    this.stars = stars;
+    this.license = license || null;
+    this.language = language;
+    this.url = url;
+  }
+
+  createTemplate() {
+    const language = this.language || 'no language';
+
+    const license = this.license
+      ? h('div', { class: 'license'}, [this.license])
+      : null;
+
+    return h('div', { class: 'project', 'data-language': language.toLowerCase()}, [
+      h('div', { class: 'inner' }, [
+        h('div', { class: 'head' }, [
+          h('div', { class: 'stars' }, [this.stars.toString()]),
+          h('div', { class: 'forks' }, [this.forks.toString()]),
+          license,
+        ]),
+        h('div', { class: 'language', 'data-language': language.toLowerCase() }, [language]),
+        h('h3', { class: 'title' }, [this.title]),
+        h('p', { class: 'description' }, [this.description]),
+        h('a', { class: 'link', href: this.url }, [this.url]),
+      ])
+    ]);
+  }
+
+}
